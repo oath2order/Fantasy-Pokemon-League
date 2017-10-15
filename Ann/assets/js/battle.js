fbb8a953164008547bfcb6320c29ede4ed5f83fb
asimeoli <asimeoli@gmail.com>
2017-10-15 11:29:55 -0400

    battle works, needs obj, need to do hndlebrs

--- Ann/assets/js/battle.js
+++ Ann/assets/js/battle.js
@@ -1,50 +1,70 @@
-teams = [{
-        "name": "team1",
+var teams = [{
+        "name": "Josh",
         "pokemon": ["pikachu", "celebi", "eevee", "charizard", "lugia", "mewtwo"]
     },
     {
-        "name": "team2",
-        "pokemon": ["pikachu", "celebi", "eevee", "charizard", "lugia", "mewtwo"]
+        "name": "Ann",
+        "pokemon": ["snorlax", "ditto", "tyranitar", "charmander", "squirtle", "gardevoir"]
     },
     {
-        "name": "team3",
-        "pokemon": ["pikachu", "celebi", "eevee", "charizard", "lugia", "mewtwo"]
+        "name": "Dante",
+        "pokemon": ["bulbasaur", "mew", "gyarados", "dragonite", "magikarp"]
     },
     {
-        "name": "team4",
-        "pokemon": ["pikachu", "celebi", "eevee", "charizard", "lugia", "mewtwo"]
+        "name": "Christina",
+        "pokemon": ["gengar", "umbreon", "lucario", "jigglypuff", "zapdos"]
     }
 ];
 
-for (var i = 0; i < teams.length; i++) {
-    var name = teams[i].name;
-    for (var i = 0; i < teams[i].pokemon.length; i++) {
-        var pokemon = teams[i].pokemon[i];
-        var url = "https://img.pokemondb.net/artwork/" + pokemon + ".jpg";
-    };
-};
 
-//beginning game
+//start game
 window.onload = function() {
-    $(".startButton").on("click", loadPokemon1(), loadPokemon2());
-    console.log(team[i].name[0]);
-    console.log(team[i.name[1]]);
+    $(".startButton").on("click", function() {
+        drawPokemon1(teams[0]);
+        drawPokemon2(teams[1]);
+        $(".startButtonDiv").html("");
+    });
 };
 
 //load pokemon
-function loadPokemon1(n) {
-    $(".placeholder").html('<div class="player1 activePokemon center-align"><img class="activeImg" src=' + url +
-        'width="300" height="280" alt=' + pokemon + ' "artwork by Ken Sugimori"></div>');
-    $(".pokeball_1").html('<img id="pokeballa" src="pokeball.png"><img id="pokeballb" src="pokeball.png"><img id="pokeballc" src="pokeball.png"><img id="pokeballd" src="pokeball.png"><img id="pokeballe" src="pokeball.png"><img id="pokeballf" src="pokeball.png">');
-    $(".forProgress_1").html('<div class="progress"><div class="determinate red" style="width: 30%"></div></div>');
-    $(".name_1").html('<div class="col s4 bar userName player2 center-align"><h2>' + teams[i].name[0] + '</h2></div>')
+function drawPokemon1(team1) {
+    //draw current player
+    $(".placeholder").html('<div class="player1 activePokemon center-align"><img class="activeImg" src=' +
+        "https://img.pokemondb.net/artwork/" + team1.pokemon[0] + ".jpg ' " +
+        'width="300" height="280" alt=' + team1.pokemon[0] + '"artwork by Ken Sugimori"></div>');
+    //pokeballs for 'to fight'
+    $(".pokeball_1").html("");
+    for (var i = 1; i < team1.pokemon.length; i++) {
+        $(".pokeball_1").append('<img id="pokeball" src="./assets/images/Pokeball.png">');
+    }
+
+    //hp bar at start
+    setHpPokemon1(100);
+
+    $(".name_1").html('<div class="bar userName player2 center-align"><h2>' + team1.name + '</h2></div>')
 }
 
-function loadPokemon2(n) {
-    $(".placeholder2").html('<div class="player2 activePokemon center-align"><img class="activeImg" src=' + url +
-        'width="300" height="280" alt=' + pokemon + ' "artwork by Ken Sugimori"></div>');
-    $(".pokeball_2").html('<img id="pokeballg" src="pokeball.png"><img id="pokeballh" src="pokeball.png"><img id="pokeballi" src="pokeball.png"><img id="pokeballj" src="pokeball.png"><img id="pokeballk" src="pokeball.png"><img id="pokeballl" src="pokeball.png">');
-    $("forProgress_2").html('<div class="progress"><div class="determinate red" style="width: 30%"></div></div>');
+function drawPokemon2(team2) {
+    //draw current player
+    $(".placeholder2").html('<div class="player2 activePokemon center-align"><img class="activeImg" src=' +
+        "https://img.pokemondb.net/artwork/" + team2.pokemon[0] + ".jpg ' " +
+        'width="300" height="280" alt=' + team2.pokemon[0] + '"artwork by Ken Sugimori"></div>');
+    //pokeballs for 'to fight'
+    $(".pokeball_2").html("");
+    for (var i = 1; i < team2.pokemon.length; i++) {
+        $(".pokeball_2").append('<img id="pokeball" src="./assets/images/Pokeball.png">');
+    }
+    //hp bar at start
+    setHpPokemon2(100);
     $(".versus").html('<h1 class="center-align">VS</h1>');
-    $(".name_2").html('<div class="col s4 bar userName player2 center-align"><h2>' + teams[i].name[1] + '</h2></div>')
+    $(".name_2").html('<div class="bar userName player2 center-align"><h2>' + team2.name + '</h2></div>')
+}
+
+//progress bars during game play
+function setHpPokemon1(HP) {
+    $(".forProgress_1").html('<div class="progress"><div class="determinate red" style="width: ' + HP + '%"></div></div>');
+}
+
+function setHpPokemon2(HP) {
+    $(".forProgress_2").html('<div class="progress"><div class="determinate red" style="width:' + HP + '%"></div></div>');
 }
